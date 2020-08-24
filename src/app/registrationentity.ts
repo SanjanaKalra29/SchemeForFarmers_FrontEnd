bb77d869fe47786d2ded1682fe1d190366f267e1
akpamitpatel <akpamitpatel@gmail.com>
2020-08-24 14:40:03 +0530

    modified the registration component

---
+++ src/app/registrationentity.ts
@@ -0,0 +1,26 @@
+export class Address{
+
+    line1:string;
+    line2:string;
+    state:string;
+    city:string;
+    pin:number;
+  
+  }
+ export class Land{
+    area:string;
+    addres:string;
+    pin2:number;
+  
+  }
+  export class Account{
+    accountno:number;
+    ifsc:string;
+  
+  }
+  export class Document{
+    addhar:number;
+    pan:string;
+    addharfile: File;
+    panfile:File;
+  }
