1c7fbd9585170712de9da5f1d9e4950dcf443be3
arc1999 <ayushrajchauhan99@gmail.com>
2020-08-26 09:06:40 +0530

    data binding check

--- src/app/farmerregistration/registerfinal.ts
+++ src/app/farmerregistration/registerfinal.ts
@@ -2,7 +2,8 @@ import { Address, Land } from './registrationentity';
 import { Register } from './farmerregistration.component';
 export class registerFinal{
 
-    register:Register;
-    address:Address;
-    land: Land;
+    register:Register=new Register();
+    address:Address=new Address();
+    land: Land= new Land();
+    
 }
