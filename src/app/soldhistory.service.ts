d6f91b1f8ca50470251cd513185f93cbfc05aede
akpamitpatel <akpamitpatel@gmail.com>
2020-08-27 10:39:34 +0530

    modified service class

--- src/app/soldhistory.service.ts
+++ src/app/soldhistory.service.ts
@@ -1,3 +1,6 @@
+import { Sold } from './viewsoldhistory/sold';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +8,11 @@ import { Injectable } from '@angular/core';
 })
 export class SoldhistoryService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  fetchAllSSoldCrops(id:number): Observable<Sold>{
+    let url = 'http://localhost:8585/bid?farmerid='+id;
+    return this.http.post<Sold>(url, id);
+
+  }
 }
